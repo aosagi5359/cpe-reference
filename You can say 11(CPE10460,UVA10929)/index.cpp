1377246ea35968f6643b3e45bd7d368a0cd20d14
aosagi5359 <aosagi5359@gmail.com>
2023-06-23 15:56:19 +0800

    a doom

--- You can say 11(CPE10460,UVA10929)/index.cpp
+++ You can say 11(CPE10460,UVA10929)/index.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 int main(){
     string s;
-    while(cin>>s && s != "0"){
+    while(cin>>s && s != "0"){ //輸入字串，字串為 0 時，結束迴圈
         long long sum[2] = {0, 0};
         for(int i = 0; i < s.length(); i++)
-            sum[i%2] += s[i] - '0';
+            sum[i%2] += s[i] - '0';     //字串字元減去0(值為48)，可得奇數值，然後將奇數與偶數分開相加>
         cout<<s<<" is"<<
-            ((sum[0] - sum[1]) % 11?" not":" ");
+            ((sum[0] - sum[1]) % 11?" not":" "); //奇數減偶數和，並減檢查兩者之差是否為11倍數
         cout<<"a multiple of 11."<<endl;
     }
 }
